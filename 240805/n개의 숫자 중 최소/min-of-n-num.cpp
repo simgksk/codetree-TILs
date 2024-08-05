bcb036a5daa56fe62236ce148492b9fea9d0f7b7
하나 <123716081+simgksk@users.noreply.github.com>
2024-08-05 14:29:05 +0900

    Title: n개의 숫자 중 최소, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/n개의 숫자 중 최소/min-of-n-num.cpp
+++ 240805/n개의 숫자 중 최소/min-of-n-num.cpp
@@ -8,21 +8,25 @@ int main() {
     int arr[100];
     int n;
     int min;
-    int cnt = 1;
+    int cnt = 0;
 
     cin >> n;
 
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
+
     min = arr[0];
-    for(int i = 0; i < n; i++){
+
+    for(int i = 1; i < n; i++){
         if(min > arr[i]){
             min = arr[i];
+        }
+    }
 
-            if(arr[i] == min){
-                cnt++;
-            }
+    for(int i = 0; i < n; i++){
+        if(arr[i] == min){
+            cnt++;
         }
     }
 
